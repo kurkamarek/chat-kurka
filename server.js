4fdde3b5a2b65c1e6fd6e75da1cfc34bba6f98c7
unknown <majek.kurka@seznam.cz>
2021-03-23 13:48:50 +0100

    třetí commit

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-const path = require('path');   
+const path = require('path');
+const http = require('http');   
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-// Set static folder
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+
+    socket.emit('message', 'Welcome to ChatCord!');
+
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');   
+    });
+
+});
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
