a1ac1d9ab7a1d2ca138d726bc7fbd33a8a27f6fa
unknown <majek.kurka@seznam.cz>
2021-03-23 16:29:47 +0100

    čtvrtý commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const http = require('http');   
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,18 +12,24 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
 
-    socket.emit('message', 'Welcome to ChatCord!');
+        socket.join(user.room);
 
-    socket.broadcast.emit('message', 'A user has joined the chat');
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');   
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                formatMessage(botName, `${user.username} has joined the chat`)
+            );
     });
-
 });
-
 const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
